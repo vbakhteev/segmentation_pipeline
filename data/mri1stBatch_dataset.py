9ba0a0bcd896f8d5d6d58eaa21e003a187724f4d
Vladislav Bakhteev <bakhteev_v@mail.ru>
2020-11-21 13:01:55 +0300

    Fix MRI dataset

--- data/mri1stBatch_dataset.py
+++ data/mri1stBatch_dataset.py
@@ -1,17 +1,26 @@
-import sys,os
+from pathlib import Path
+from typing import Union, Callable
+
 import cv2
 from sklearn.model_selection import train_test_split
+
 from data.base_dataset import BaseDataset
-from pathlib import Path
 
 
 class Mri1stBatchDataset(BaseDataset):
-    '''
+    """
     Data resides here: /mercurySOFS/Medical/MKDC_stroke_CT_MRI/1stBatchWithMasks
-    '''
-    def __init__(self, root, samples_per_epoch, is_train, patient_names, transforms=None, augmentations=None):
-        BaseDataset.__init__(self, root, samples_per_epoch, transforms, is_train)
-        self.augmentations = augmentations
+    """
+
+    def __init__(
+        self,
+        root: Union[str, Path],
+        samples_per_epoch: int,
+        transforms: Callable,
+        is_train: bool,
+        patient_names: list,
+    ):
+        super().__init__(root, samples_per_epoch, transforms, is_train)
         self.paths = []
         for name in self.root.iterdir():
             if name.stem in patient_names:
@@ -29,20 +38,14 @@ class Mri1stBatchDataset(BaseDataset):
         image = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
         mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
 
-        if self.augmentations is not None:
-            sample = self.augmentations(image=image, mask=mask)
-            image = sample["image"]
-            mask = sample["mask"]
-
-        if self.transforms is not None:
-            sample = self.transforms(image=image, mask=mask)
-            image = sample["image"]
-            mask = sample["mask"]
+        sample = self.transforms(image=image, mask=mask)
+        image = sample["image"]
+        mask = sample["mask"]
 
         return {"image": image, "mask": mask}
 
     @staticmethod
-    def prepare_data(root=None, cfg=None) -> dict:
+    def prepare_data(cfg=None, root=None) -> dict:
         """Read train/validation split
         Returns:
             a dictionary of data that will be used for creation of dataset and dataloader
@@ -51,9 +54,9 @@ class Mri1stBatchDataset(BaseDataset):
             root = Path(cfg.dataset.root)
         else:
             root = Path(root)
-        dirnames = [p.stem for p in root.iterdir() if not p.stem.startswith('.')]
+        dirnames = [p.stem for p in root.iterdir() if not p.stem.startswith(".")]
 
-        train_filenames, valid_filenames = train_test_split(dirnames)
+        train_filenames, valid_filenames = train_test_split(dirnames, random_state=42)
 
         result = dict(
             train={"patient_names": train_filenames},
@@ -64,9 +67,9 @@ class Mri1stBatchDataset(BaseDataset):
     @staticmethod
     def read_patient(root):
         root = Path(root)
-        mask_dir = root / 'masks'
-        img_dir = next(root.glob('*DWI*'))
-        filenames = mask_dir.glob('*.png')
+        mask_dir = root / "masks"
+        img_dir = next(root.glob("*DWI*"))
+        filenames = mask_dir.glob("*.png")
         ret = []
         for name in filenames:
             img_path = img_dir / name.name
