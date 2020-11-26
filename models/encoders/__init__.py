7a63f99c2cd4297bd1973b36fd5c46d3bd693423
Vladislav Bakhteev <bakhteev_v@mail.ru>
2020-11-26 12:52:54 +0300

    Small refactoring

--- models/encoders/__init__.py
+++ models/encoders/__init__.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-from models.utils import get_layers_by_dim
+from models.utils import change_num_channels_first_conv
 from .backbones_2d import get_backbone_2d
 from .backbones_3d import get_backbone_3d
 
@@ -14,37 +14,10 @@ def get_encoder(encoder_name, n_dim, num_channels):
     else:
         raise NotImplementedError(f"Models with n_dim={n_dim} are not supported")
 
-    if num_channels == 3:
-        pass
-    elif num_channels == 1:
-        named_conv = next(model.named_parameters())
-        name = named_conv[0].split(".")[:-1]
-        w = named_conv[1]  # (out_filters, 3, k, k) or (out_filters, 3, k, k, k)
-
-        nn_module = model
-        for n in name[:-1]:
-            nn_module = getattr(nn_module, n)
-        first_conv = next(nn_module.children())
-
-        Conv = get_layers_by_dim(n_dim)
-        setattr(
-            nn_module,
-            name[-1],
-            Conv(
-                1,
-                w.shape[0],
-                kernel_size=w.shape[2:],
-                stride=first_conv.stride,
-                padding=first_conv.padding,
-                bias=first_conv.bias,
-                dilation=first_conv.dilation,
-                groups=first_conv.groups,
-            ),
-        )
-        w = torch.sum(w, dim=1, keepdim=True)
-        getattr(nn_module, name[-1]).weight = nn.Parameter(w)
-
-    else:
-        raise NotImplementedError(f"Num channels={num_channels} is not implemented")
+    model = change_num_channels_first_conv(
+        model=model,
+        n_dim=n_dim,
+        num_channels=num_channels,
+    )
 
     return model
